0901c918eb521f99a18667ad4b8b5fdc17e49ac7
Alex <undersundernew@gmail.com>
2023-05-02 21:28:52 +0300

    fix: update animation

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ const keySymbols = [
 "tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", ["{", "["], ["}", "]"], ["|", "\\"],
 "caps lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", [":", ";"], ['"', "'"], "return",
 "shift", "Z", "X", "C", "V", "B", "N", "M", ["<", ","], [">", "."], ["?", "/"], "shift",
-"fn", "control", "option", "command", "space", "command", "option", "left", "up", "down", "rigth"]
+"fn", "control", "option", "command", "space", "command", "option", "left", "up", "down", "right"]
 
 const enterSymbols = [ 
   ["~", "`"], ["!", "1"], ["@", "2"], ["#", "3"], ["$", "4"], ["%", "5"], ["^", "6"], ["&", "7"],
-  ["*", "8"], ["(", "9"], [")", "0"], ["_", "-"], ["+", "="], "delete", "tab", ["Q", "q"], 
+  ["*", "8"], ["(", "9"], [")", "0"], ["_", "-"], ["+", "="], "Backspace", "Tab", ["Q", "q"], 
   ["W", "w"], ["E", "e"], ["R", "r"], ["T", "t"], ["Y", "y"], ["U", "u"], ["I", "i"], ["O", "o"], 
-  ["P", "p"], ["{", "["], ["}", "]"], ["|", "\\"],"caps lock", ["A", "a"], ["S", "s"], ["D", "d"],
+  ["P", "p"], ["{", "["], ["}", "]"], ["|", "\\"], "CapsLock", ["A", "a"], ["S", "s"], ["D", "d"],
   ["F", "f"], ["G", "g"], ["H", "h"], ["J", "j"], ["K", "k"], ["L", "l"], [":", ";"], ['"', "'"],
-  "return", "shift", ["Z", "z"], ["X", "x"], ["C", "c"], ["V", "v"], ["B", "b"], ["N", "n"],
-  ["M", "m"], ["<", ","], [">", "."], ["?", "/"], "shift",
-  "fn", "control", "option", "command", "space", "command", "option", "left", "up", "down", "rigth"]
+  "Enter", "Shift", ["Z", "z"], ["X", "x"], ["C", "c"], ["V", "v"], ["B", "b"], ["N", "n"],
+  ["M", "m"], ["<", ","], [">", "."], ["?", "/"], "Shift",
+  "fn", "Control", "Alt", "Meta", " ", "Meta", "Alt", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight"]
 
 for(let i=0; i<keySymbols.length; i++){
   const button = document.createElement('button')
@@ -36,45 +36,74 @@ for(let i=0; i<keySymbols.length; i++){
     if(Array.isArray(keySymbols[i]))
     button.innerHTML = (`${keySymbols[i][0]}<br>${keySymbols[i][1]}`)
 
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', (event)=>{
       this.button = button;
       if(Array.isArray(enterSymbols[i]) && !document.querySelector('.caps_active') && !document.querySelector('.shift_active')){
       textarea.value += enterSymbols[i][1]
+      this.button.classList.add('press_button')
+      setTimeout(() => {
+        button.classList.remove('press_button')
+      }, 200);
       }
       if(Array.isArray(enterSymbols[i]) && (document.querySelector('.caps_active') || document.querySelector('.shift_active'))){
         textarea.value += enterSymbols[i][0]
+        this.button.classList.add('press_button')
+        setTimeout(() => {
+          button.classList.remove('press_button')
+        }, 200);
         }
       else{
-        if(enterSymbols[i] === "space"){
+        if(enterSymbols[i] === " "){
         textarea.value += ' '
+        this.button.classList.add('press_button')
+        setTimeout(() => {
+          button.classList.remove('press_button')
+        }, 200);
         }
-        if(enterSymbols[i] === "tab"){
+        if(enterSymbols[i] === "Tab"){
           textarea.value += '    '
+          this.button.classList.add('press_button')
+          setTimeout(() => {
+            button.classList.remove('press_button')
+          }, 200);
           }
-        if(enterSymbols[i] === "caps lock"){
+        if(enterSymbols[i] === "CapsLock"){
           button.classList.toggle('caps_active')
           }  
-        if(enterSymbols[i] === "delete"){
+        if(enterSymbols[i] === "Backspace"){
           textarea.value = textarea.value.slice(0, textarea.value.length-1)
-          console.log(this.button)
+          this.button.classList.add('press_button')
+          setTimeout(() => {
+            button.classList.remove('press_button')
+          }, 200);
           }  
-        if(enterSymbols[i] === "shift"){
+        if(enterSymbols[i] === "Shift"){
           button.classList.toggle('shift_active')
           }  
-        if(enterSymbols[i] === "return"){
+        if(enterSymbols[i] === "Enter"){
         textarea.value = `${textarea.value}\n`
+        this.button.classList.add('press_button')
+        setTimeout(() => {
+          button.classList.remove('press_button')
+        }, 200);
         }
-        if(enterSymbols[i] === "left"){
+        if(enterSymbols[i] === "ArrowLeft"||enterSymbols[i] === "ArrowUp"|| enterSymbols[i] === "ArrowDown"
+        ||enterSymbols[i] === "ArrowRight"|| enterSymbols[i] === "Meta"||enterSymbols[i] === "Alt"
+        ||enterSymbols[i] === "Control" ){
+          this.button.classList.add('press_button')
+          setTimeout(() => {
+            button.classList.remove('press_button')
+          }, 200);
         }
       }
     })
 
     textarea.addEventListener('keydown', (event)=>{
       this.button = button;
-      if((event.key.toUpperCase() === keySymbols[i][0]
-      ||event.key.toUpperCase() === keySymbols[i][1]
-      ||event.key.toUpperCase() === keySymbols[i]
-      )){
+      if( event.key === enterSymbols[i][0] && Array.isArray(enterSymbols[i])
+      ||event.key === enterSymbols[i][1]  && Array.isArray(enterSymbols[i])
+      ||(event.key === enterSymbols[i] && !Array.isArray(enterSymbols[i]))
+       ){
         this.button.classList.add('press_button')
         setTimeout(() => {
           button.classList.remove('press_button')
@@ -82,3 +111,7 @@ for(let i=0; i<keySymbols.length; i++){
       }
     })
 }
+
+textarea.addEventListener('keydown', (event)=>{
+  console.log(event.key)
+})
